f5bd94cb15c7704cb32ec5e29ca852cd2b586bf4
RodrigoDiGiulio <145570964+RodrigoDiGiulio@users.noreply.github.com>
2025-02-07 21:26:32 -0400

    PostGame Responsive
    Ya es responsive y con nuevo diseño

--- src/pages/overlay/postgame/PostGame.tsx
+++ src/pages/overlay/postgame/PostGame.tsx
@@ -10,7 +10,8 @@ import blueScoreBlock from './assets/blueScoreBlock.svg';
 import orangeScoreBlock from './assets/orangeScoreBlock.svg';
 import orangeMatchWonBlock from './assets/orangeMatchWonBlock.svg';
 import blueMatchWonBlock from './assets/blueMatchWonBlock.svg';
-import matchBlock from './assets/matchBlock.svg';
+import orangeMatchLostBlock from './assets/orangeMatchLostBlock.svg';
+import blueMatchLostBlock from './assets/blueMatchLostBlock.svg';
 
 const PostGame: Component = () => {
   interface PlayerData {
@@ -49,13 +50,13 @@ const PostGame: Component = () => {
   const teams: TeamData[] = [
     {
       id: 0,
-      name: "Equipo A",
+      name: "Equipo Exagerado A",
       pfp: wallpaperImage,
       players: 3
     },
     {
       id: 0,
-      name: "Equipo B",
+      name: "Equipo Exagerado B",
       pfp: wallpaperImage,
       players: 3
     }
@@ -120,60 +121,70 @@ const PostGame: Component = () => {
 
   return (
     <div class="w-screen h-screen" style={{ background: `url(${bg})` , "background-size": 'cover', "background-position": 'center' }}>
-      <div class="w-[80vw] text-center z-10 mx-auto">
-        <Grid cols={7} class='pb-[0vh] pt-[10vh]'>{teams.map((team, index) => (
+      <div class="w-[90vw] text-center z-10 mx-auto">
+        <div class='text-gray-300 text-[1.2vw] pb-[0vw] pt-[1vw] font-bold opacity-50'>
+          {'CAMPEONATO HELLO WORLD | CARACAS OPEN #1 | LATAM | PLAYOFFS | WEEK 8'}
+        </div>
+        <Grid cols={7} class='pb-[0vw] pt-[1vw]'>{teams.map((team, index) => (
           <>
             {index === 1 ? (
               <>
-                <Col>
-                  <div class='leading-none m-0 p-0'>
-                    <span class='text-[6vh]'>JUEGO</span><br/>
-                    <span class='text-[4vh]'>{game[0].id}</span>
+                <Col></Col>
+                {/* <Col class="relative">
+                  <div class="absolute inset-0">
+                    <div class="text-white text-center text-[2.5vw] leading-none" style={{ top: '50%', left: '50%', transform: 'translate(0%, 0%)'}}>
+                      <span class='text-[1.8vw]'>JUEGO</span><br/>
+                      <span class='text-[1.4vw]'>{game[0].id}</span>
+                    </div>
                   </div>
-                </Col>
-                <Col class='relative'>
-                  <img src={orangeScoreBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <div class='text-orange-500 text-center text-[8vh] mb-[1vh] pr-[8vw] font-bold'>
+                </Col> */}
+                <Col class="relative">
+                  <img src={orangeScoreBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-[color:#ff8a15] text-center text-[4vw] font-bold" style={{ top: '50%', left: '50%', transform: 'translate(-75%, -10%)'}}>
                       {game[0].score2}
                     </div>
                   </div>
                 </Col>
-                <Col class='relative'>
-                  <img src={orangeNameBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <div class='text-white text-center text-[5vh] mb-[1vh]'>
+                <Col class="relative">
+                  <img src={orangeNameBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-white text-center text-[2.5vw] text-nowrap" style={{ top: '50%', left: '50%', transform: 'translate(-70%, 15%)'}}>
                       {team.name}
                     </div>
                   </div>
                 </Col>
-                <Col class='relative'>
-                  <img src={orangeLogoBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <img src={team.pfp} class="w-fit h-fit scale-50 mb-[1vh]"/>
+                <Col class="relative">
+                  <img src={orangeLogoBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-white text-center text-[2.5vw] text-nowrap" style={{ top: '50%', left: '50%', transform: 'translate(0%, -15%)'}}>
+                      <img src={team.pfp} class="w-fit h-fit scale-50 mb-[1vh]"/>
+                    </div>
                   </div>
                 </Col>
               </>
             ) : (
               <>
-                <Col class='relative'>
-                  <img src={blueLogoBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <img src={team.pfp} class="w-fit h-fit scale-50 mb-[1vh]"/>
+                <Col class="relative">
+                  <img src={blueLogoBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-white text-center text-[2.5vw] text-nowrap" style={{ top: '50%', left: '50%', transform: 'translate(0%, -15%)'}}>
+                      <img src={team.pfp} class="w-fit h-fit scale-50 mb-[1vh]"/>
+                    </div>
                   </div>
                 </Col>
-                <Col class='relative'>
-                  <img src={blueNameBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <div class='text-white text-center text-[5vh] mb-[1vh]'>
+                <Col class="relative">
+                  <img src={blueNameBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-white text-center text-[2.5vw] text-nowrap" style={{ top: '50%', left: '50%', transform: 'translate(0%, 15%)'}}>
                       {team.name}
                     </div>
                   </div>
                 </Col>
-                <Col class='relative'>
-                  <img src={blueScoreBlock} class='w-auto h-auto object-cover inset-0 z-0'/>
-                  <div class="absolute inset-0 flex items-center justify-center z-10">
-                    <div class='text-blue-500 text-center text-[8vh] mb-[1vh] pl-[8vw] font-bold'>
+                <Col class="relative">
+                  <img src={blueScoreBlock} class="w-auto h-auto object-cover absolute inset-0 z-0" alt={team.name} />
+                  <div class="absolute inset-0 z-10">
+                    <div class="text-[color:#22b0ff] text-center text-[4vw] font-bold" style={{ top: '50%', left: '50%', transform: 'translate(75%, -10%)'}}>
                       {game[0].score1}
                     </div>
                   </div>
@@ -183,64 +194,70 @@ const PostGame: Component = () => {
           </>
           ))}
         </Grid>
-        <div class="w-[80vw] h-[4vh] mx-auto flex justify-center items-center">
-          <div class="flex items-center">
-            <img class="size-[8vh]" src={matchBlock}/>
-            <img class="size-[8vh]" src={matchBlock}/>
-            <img class="size-[8vh]" src={blueMatchWonBlock}/>
-            <img class="size-[8vh]" src={blueMatchWonBlock}/>
-          </div>
-          <div class="px-[7.8vw]" textContent={'Mejor de 7'}></div>
-          <div class="flex items-center">
-            <img class="size-[8vh]" src={orangeMatchWonBlock}/>
-            <img class="size-[8vh]" src={orangeMatchWonBlock}/>
-            <img class="size-[8vh]" src={orangeMatchWonBlock}/>
-            <img class="size-[8vh]" src={matchBlock}/>
-          </div>
-        </div>
-        <Grid cols={7} class="pb-[5vh] pt-[5vh] text-[4vh] font-semibold text-">
+        <Grid cols={3} class='pb-[0vw] pt-[5vw]'>
+          <Col class="inset-0 z-10 relative flex items-center justify-center" style={{ top: '0%', left: '0%', transform: 'translate(55.4%, 10%)'}}>
+            <img src={blueMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(60%, 0%)'}}/>
+            <img src={blueMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(40%, 0%)'}}/>
+            <img src={blueMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(20%, 0%)'}}/>
+            <img src={blueMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(0%, 0%)'}}/>
+          </Col>
+          <Col class="relative">
+            <div class="absolute inset-0 z-10">
+              <div class="text-white text-center text-[1.5vw] text-nowrap" style={{ top: '50%', left: '50%', transform: 'translate(0%, -10%)'}}>
+                {'JUEGO 2'}
+              </div>
+            </div>
+          </Col>
+          <Col class="inset-0 z-10 relative flex items-center justify-center" style={{ top: '0%', left: '0%', transform: 'translate(-63%, 10%)'}}>
+            <img src={orangeMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(60%, 0%)'}}/>
+            <img src={orangeMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(40%, 0%)'}}/>
+            <img src={orangeMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(20%, 0%)'}}/>
+            <img src={orangeMatchWonBlock} class="max-w-[5vw] max-h-[1.5vw] object-contain" style={{ top: '50%', left: '50%', transform: 'translate(0%, 0%)'}}/>
+          </Col>
+        </Grid>
+        <Grid cols={7} class="pb-[1vw] pt-[1vw] text-[2vw] font-semibold text-">
           {players.map((player, index) => {
             if (index < 3) {
               return (
-                <Col span={1} class='pb-[1vh] pt-[1vh]'>
-                  <div class='pb-[1vh] pt-[1vh] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-blue-500 to-90%'>{player.name}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.score}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.goals}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.assists}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.shots}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.saves}</div>
+                <Col span={1} class='pb-[1vw] pt-[1vw]'>
+                  <div class='pb-[1vw] pt-[1vw] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-[color:#22b0ff] to-90%'>{player.name}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.score}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.goals}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.assists}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.shots}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.saves}</div>
                 </Col>
               );
             } else if (index === 3) {
               return (
                 <>
-                  <Col span={1} class='pb-[1vh] pt-[1vh]'>
-                    <div class='pb-[1vh] pt-[1vh]'>Jugadores</div>
-                    <div class='pb-[1vh] pt-[1vh]'>Puntos</div>
-                    <div class='pb-[1vh] pt-[1vh]'>Goles</div>
-                    <div class='pb-[1vh] pt-[1vh]'>Asistencias</div>
-                    <div class='pb-[1vh] pt-[1vh]'>Tiros</div>
-                    <div class='pb-[1vh] pt-[1vh]'>Salvadas</div>
+                  <Col span={1} class='pb-[1vw] pt-[1vw]'>
+                    <div class='pb-[1vw] pt-[1vw]'>Jugadores</div>
+                    <div class='pb-[1vw] pt-[1vw]'>Puntos</div>
+                    <div class='pb-[1vw] pt-[1vw]'>Goles</div>
+                    <div class='pb-[1vw] pt-[1vw]'>Asistencias</div>
+                    <div class='pb-[1vw] pt-[1vw]'>Tiros</div>
+                    <div class='pb-[1vw] pt-[1vw]'>Salvadas</div>
                   </Col>
-                  <Col span={1} class='pb-[1vh] pt-[1vh]'>
-                    <div class='pb-[1vh] pt-[1vh] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-orange-500 to-90%'>{player.name}</div>
-                    <div class='pb-[1vh] pt-[1vh]'>{player.score}</div>
-                    <div class='pb-[1vh] pt-[1vh]'>{player.goals}</div>
-                    <div class='pb-[1vh] pt-[1vh]'>{player.assists}</div>
-                    <div class='pb-[1vh] pt-[1vh]'>{player.shots}</div>
-                    <div class='pb-[1vh] pt-[1vh]'>{player.saves}</div>
+                  <Col span={1} class='pb-[1vw] pt-[1vw]'>
+                    <div class='pb-[1vw] pt-[1vw] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-[color:#ff8a15] to-90%'>{player.name}</div>
+                    <div class='pb-[1vw] pt-[1vw]'>{player.score}</div>
+                    <div class='pb-[1vw] pt-[1vw]'>{player.goals}</div>
+                    <div class='pb-[1vw] pt-[1vw]'>{player.assists}</div>
+                    <div class='pb-[1vw] pt-[1vw]'>{player.shots}</div>
+                    <div class='pb-[1vw] pt-[1vw]'>{player.saves}</div>
                 </Col>
                 </>
               );
             } else {
               return (
-                <Col span={1} class='pb-[1vh] pt-[1vh]'>
-                  <div class='pb-[1vh] pt-[1vh] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-orange-500 to-90%'>{player.name}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.score}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.goals}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.assists}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.shots}</div>
-                  <div class='pb-[1vh] pt-[1vh]'>{player.saves}</div>
+                <Col span={1} class='pb-[1vw] pt-[1vw]'>
+                  <div class='pb-[1vw] pt-[1vw] bg-gradient-to-b from-transparent from-0% via-transparent via-90% to-orange-500 to-90%'>{player.name}</div>
+                  <div class='pb-[1vw] pt-[1vw] '>{player.score}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.goals}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.assists}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.shots}</div>
+                  <div class='pb-[1vw] pt-[1vw]'>{player.saves}</div>
                 </Col>
               );
             }
