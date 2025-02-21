5fce881c73168df2a4ebfa4f5e91fb2fb992623c
DAHDOR <diegoahm1510@gmail.com>
2025-02-21 10:43:34 -0400

    add ws subs for game init and game end (automatic transitions)

--- src/pages/overlay/index.tsx
+++ src/pages/overlay/index.tsx
@@ -1,4 +1,5 @@
-import { Route } from '@solidjs/router'
+import { useWS } from '@/contexts/ws'
+import { Route, useNavigate } from '@solidjs/router'
 import { Component, ParentProps } from 'solid-js'
 import InGame from './ingame'
 import NextUp from './nextup'
@@ -15,6 +16,20 @@ const Layout: Component<ParentProps> = props => {
 }
 
 const OverlayPages = () => {
+  const ws = useWS()
+
+  const navigate = useNavigate()
+
+  ws.subscribe('game', 'initialized', data => {
+    console.log('game:initialized', data)
+    navigate('/overlay/ingame')
+  })
+
+  ws.subscribe('game', 'match_ended', data => {
+    console.log('game:match_ended', data)
+    navigate('/overlay/winner') // TODO: winner page, it has to navigate to postgame after finishing
+  })
+
   return (
     <Route path="/overlay" component={Layout}>
       <Route path="/phase" component={Phase} />
