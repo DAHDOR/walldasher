0fa026f76b5f5c42dbb67e0eede3ef6dba52d4d7
DAHDOR <diegoahm1510@gmail.com>
2025-03-05 00:52:51 -0400

    db fix & single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,14 +15,26 @@ use tauri_plugin_store::StoreExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let port: u16 = 9527;
-    tauri::Builder::default()
-        .plugin(tauri_plugin_persisted_scope::init())
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
-                .add_migrations("sqlite:test3.db", db::migrations::get_migrations())
+                .add_migrations("sqlite:test4.db", db::migrations::get_migrations())
                 .build(),
         )
         .plugin(tauri_plugin_store::Builder::new().build())
