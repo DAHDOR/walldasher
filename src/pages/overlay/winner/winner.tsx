9c0f3e9da03fdfa47c6d28fae9f7bec75576fcf1
RodrigoDiGiulio <145570964+RodrigoDiGiulio@users.noreply.github.com>
2025-03-09 13:30:27 -0400

    Beta Winner

--- src/pages/overlay/winner/winner.tsx
+++ src/pages/overlay/winner/winner.tsx
@@ -1,54 +1,140 @@
 import { useSnapshot } from '@/contexts/snapshot';
-import { Component, createEffect, onMount, createSignal } from 'solid-js';
-import orangeVideo from './assets/orangeWinning.webm';
+import { Component, createSignal, onMount } from 'solid-js';
+import gsap from 'gsap';
+
+let Blue = '22b0ff';
+let LightBlue = '36d0ff';
+let Orange = 'ff8a15';
+let LightOrange = 'ffbc00';
+
+let ColorA = Blue;
+let ColorB = LightBlue;
+
+interface AnimatedSVGProps {
+  setRef: (el: SVGSVGElement) => void
+}
+
+const LineAtoRight: Component<AnimatedSVGProps> = ({ setRef }) => {
+  return (
+    <svg ref={setRef} width="300" height="1080" style="position: absolute;">
+      <g>
+        <path
+          style={`fill:#${ColorA};stroke-width:20;stroke-linecap:square;stroke-miterlimit:5.8;fill-opacity:1`}
+          d="M 0,0 150,540 0,1080 H 150 L 300,540 150,0 Z"
+          id="path1"
+        />
+      </g>
+    </svg>
+  );
+};
+
+const LineBtoRight: Component<AnimatedSVGProps> = ({ setRef }) => {
+  return (
+    <svg ref={setRef} width="300" height="1080" style="position: absolute;">
+      <g>
+        <path
+          style={`fill:#${ColorB};stroke-width:20;stroke-linecap:square;stroke-miterlimit:5.8;fill-opacity:1`}
+          d="M 0,0 150,540 0,1080 H 150 L 300,540 150,0 Z"
+          id="path1"
+        />
+      </g>\
+    </svg>
+  );
+};
+
+const LineAtoLeft: Component<AnimatedSVGProps> = ({ setRef }) => {
+  return (
+    <svg ref={setRef} width="300" height="1080" style="position: absolute;">
+        <g transform="scale(-1 1) translate(-300 0)">
+            <path
+                style={`fill:#${ColorA};stroke-width:20;stroke-linecap:square;stroke-miterlimit:5.8;fill-opacity:1`}
+                d="M 0,0 150,540 0,1080 H 150 L 300,540 150,0 Z"
+                id="path1"
+            />
+        </g>
+    </svg>
+  );
+};
+
+const LineBtoLeft: Component<AnimatedSVGProps> = ({ setRef }) => {
+  return (
+    <svg ref={setRef} width={300} height="1080" style="position: absolute;">
+        <g transform="scale(-1 1) translate(-300 0)">
+            <path
+                style={`fill:#${ColorB};stroke-width:20;stroke-linecap:square;stroke-miterlimit:5.8;fill-opacity:1`}
+                d="M 0,0 150,540 0,1080 H 150 L 300,540 150,0 Z"
+                id="path1"
+            />
+        </g>
+    </svg>
+  );
+};
 
 const PostGame: Component = () => {
   const stats = useSnapshot();
-  const snapState = () => stats();
-  const [videoLoaded, setVideoLoaded] = createSignal(false);
-  createEffect(() => {
+  const [svgElementAtoRight, setSvgElementAtoRight] = createSignal<SVGSVGElement | undefined>();
+  const [svgElementBtoRight, setSvgElementBtoRight] = createSignal<SVGSVGElement | undefined>();
+  const [svgElementAtoLeft, setSvgElementAtoLeft] = createSignal<SVGSVGElement | undefined>();
+  const [svgElementBtoLeft, setSvgElementBtoLeft] = createSignal<SVGSVGElement | undefined>();
+
+  onMount(() => {
+    enter();
   });
 
-  const handleVideoLoaded = () => {
-    setVideoLoaded(true);
+  const start = gsap.timeline({ paused: true , repeat: -1});
+
+  const setStart = () => {
+    gsap.set(svgElementAtoRight(),{ x: -300})
+    gsap.set(svgElementBtoRight(),{ x: -450})
+    gsap.set(svgElementAtoLeft(),{ x: 0})
+    gsap.set(svgElementBtoLeft(),{ x: 150})
+    start.to(svgElementAtoRight(), { x: 960, duration: 5, ease: 'linear' }, 0);
+    start.to(svgElementBtoRight(), { x: 810, duration: 5, ease: 'linear' }, 0);
+    start.to(svgElementAtoLeft(), { x: -1260, duration: 5, ease: 'linear' }, 0);
+    start.to(svgElementBtoLeft(), { x: -1110, duration: 5, ease: 'linear' }, 0);
+    start.play();
   };
 
-  console.log("Hi1")
-  document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hi2");
-    const fadeDiv1 = document.getElementById('fadeDiv1');
-    const fadeDiv2 = document.getElementById('fadeDiv2');
-    if (fadeDiv1 && fadeDiv2) {
-      setTimeout(function() {
-        fadeDiv1.classList.add('opacity-100');
-        fadeDiv2.classList.add('opacity-100');
-        console.log("Hi3");
-      }, 1000);
-    } else {
-      console.error("fadeDiv1 or fadeDiv2 not found!");
-    }
-  });
+  const enter = () => {
+    setStart();
+  };
 
   return (
     <>
-      <div class="absolute w-[1920px] h-[1080px] flex items-center justify-center overflow-hidden z-0">
-        <video
-          src={orangeVideo}
-          autoplay={true}
-          loop
-          class="z-10 w-auto min-w-full min-h-full max-w-none"
-          onloadeddata={handleVideoLoaded}
-          muted
-        >
-        </video>
-      </div>
-      <div class='absolute w-[1920px] text-center pt-[380px] text-[100px] z-10'>
-        <div id="fadeDiv1" class="opacity-0 transition-opacity delay-1000 duration-500 ease-in-out text-black font-[chivo] uppercase font-bold">
-          Winner
+      <div class="absolute w-[1920px] h-[1080px]">
+        <div class='overflow-hidden flex flex-row absolute w-[960px] h-[1080px] bg-red-800'>
+          {Array.from({ length: 2 }, (_, index) => {
+            if (index % 2 === 0){
+              return(
+                <div>
+                  <LineAtoRight setRef={setSvgElementAtoRight}/>
+                </div>
+              )
+            } else {
+              return(
+                <div>
+                  <LineBtoRight setRef={setSvgElementBtoRight}/>
+                </div>
+              )
+            }
+          })}
         </div>
-        <div id="fadeDiv2" class="opacity-0 transition-opacity delay-1000 duration-500 ease-in-out text-black font-[chivo] uppercase">
-          {/* hello world */}
-          {stats().winner}
+        <div class='overflow-hidden flex flex-row-reverse absolute w-[960px] h-[1080px] translate-x-[960px] bg-green-400'>
+          {Array.from({ length: 2 }, (_, index) => {
+            if (index % 2 === 0){
+              return(
+                <div>
+                  <LineAtoLeft setRef={setSvgElementAtoLeft}/>
+                </div>
+              )
+            } else {
+              return(
+                <div>
+                  <LineBtoLeft setRef={setSvgElementBtoLeft}/>
+                </div>
+              )
+            }
+          })}
         </div>
       </div>
     </>
